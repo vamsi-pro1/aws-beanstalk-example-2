8e05e43436a57a03c27214f332d6a84b8b5da4b0
kpsfdc1 <38474055+kpsfdc1@users.noreply.github.com>
2021-02-07 10:03:50 +0530

    code remodify

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "remodify the source code..";
+        return "remodify the source code..remodify";
     }
 }
